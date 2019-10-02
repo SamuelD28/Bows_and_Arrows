841c2dd2772602851e511308a7021989ec3fa044
1187247 <1187247@cstjean.qc.ca>
2019-10-02 17:48:56 -0400

    Spawner + tipi

---
+++ Bullets&Arrows/Assets/PanelOpener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+
+    private void OnMouseDown()
+    {
+        OpenPanel();
+    }
+
+    public void OpenPanel()
+    {
+        if (!Panel.activeSelf)
+        {
+            Panel.SetActive(true);
+        }
+        else
+
+            Panel.SetActive(false);
+    }
+}
---
+++ Bullets&Arrows/Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Object;
+    public GameObject Troop;
+
+    public void Spawn()
+    {
+        var troop = Instantiate(this.Troop) as GameObject;
+
+        var x = Object.transform.position.x - Random.Range(-7, 7);
+        var y = 1;
+        var z = Object.transform.position.z - Random.Range(-7, 7);
+
+        var vec = new Vector3(x, y, z);
+        troop.transform.position = vec;
+    }
+}
